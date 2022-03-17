a9d9a3605bd347046f56a2e0602b24c1911ac7ca
jcostateixeira <jose.a.teixeira+github@gmail.com>
2022-03-17 14:36:59 +0000

    stuff

---
+++ fromfsh_to_xml.sh
@@ -0,0 +1,13 @@
+
+sushi .
+rm -rf fsh-generated/xml
+mkdir -p fsh-generated/xml
+
+for filename in fsh-generated/resources/$1*.json; do
+echo "Converting $filename ...."
+echo ". . . . . "
+    f="$(basename -- $filename)"
+    b=${f%.*}
+    java -jar validator_cli.jar  fsh-generated/resources/"$b".json -version 4.0.1  -convert -output fsh-generated/xml/"$b".xml
+done
+
